113c71dce9bbbdf759e6596b9800c5a09461fd79
Yuvraj Jwala <74554809+yuvrajjwala@users.noreply.github.com>
2021-04-29 02:03:46 +0530

    Add files via upload

---
+++ password.js
@@ -0,0 +1,8 @@
+function myfun() {
+    if (document.form.password.value === "stayhappy") {
+        alert("login successfully");
+        window.location = "https://jgoso.com"
+    } else {
+        alert("invalid");
+    }
+}
