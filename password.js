0e72e99796489c2c08f7c0f4a187aafb3b966d97
Yuvraj Jwala <74554809+yuvrajjwala@users.noreply.github.com>
2021-04-29 02:11:02 +0530

    Update password.js

--- password.js
+++ password.js
@@ -1,8 +1,12 @@
+// password is = stayhappy
+
+//This will redirect you to jgoso.com
+
 function myfun() {
     if (document.form.password.value === "stayhappy") {
         alert("login successfully");
         window.location = "https://jgoso.com"
     } else {
-        alert("Invalid Password !");
+        alert("Invalid Password ! Try again.");
     }
 }
